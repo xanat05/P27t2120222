5d690ac93fd6e5262192c992a9d7e61a9ff3ea03
MITZI DANIELA HERNANDEZ SANCHEZ <56007153+MavisB@users.noreply.github.com>
2022-09-08 19:38:06 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/itics/p27t2120222/MainActivity.java
@@ -0,0 +1,13 @@
+package mx.edu.tesoem.itics.p27t2120222;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
